f3cf71dabb10e87e65fc3696494320d25de080db
ewascent@gmail.com <ewascent@gmail.com>
2017-05-01 23:31:57 -0700

    small changes

--- js/angular.js
+++ js/angular.js
@@ -1,8 +1,10 @@
 /*jslint white:true*/
 /*global angular, XMLHttpRequest, console, XDomainRequest*/
 var myApp = angular.module('myApp', []);
+var api = 'https://api.data.gov/nrel/alt-fuel-stations/v1.json?&api_key=';
 var api_key = 'pCvPNRrbY4MMSEOwjbHEvrncKyizOgikI90rLoPV';
-var api = 'https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=100&api_key=pCvPNRrbY4MMSEOwjbHEvrncKyizOgikI90rLoPV';
+var api_limit = '&limit=30'
+var api_state = '&state=AL,TX,CA'
 
 function xreq(method, uri, withCredentials, contentType){
   'use strict';
@@ -22,23 +24,21 @@ myApp.controller('mainController', ['$scope', function ($scope) {
 
 myApp.controller('dataController', ['$scope', function($scope) {
     'use strict';
-    var xhr = xreq("GET", 'https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=10&api_key=pCvPNRrbY4MMSEOwjbHEvrncKyizOgikI90rLoPV', false, 'application/json');
+    //var xhr = xreq("GET", 'https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=10&api_key=pCvPNRrbY4MMSEOwjbHEvrncKyizOgikI90rLoPV', false, 'application/json');
+    var xhr = xreq("GET", api.concat(api_key).concat(api_state).concat(api_limit), false, 'application/json');
     xhr.send();
-    var formatter = angular.fromJson(xhr.response);
-    $scope.fuels = formatter.station_counts.fuels;
-    $scope.stations = formatter.fuel_stations;
-    $scope.data = formatter;
 
-    var drones = xreq("GET", 'http://api.dronestre.am/data', true, 'application/json');
-    drones.send();
-    $scope.drones = drones;
+    var formatter = angular.fromJson(xhr.response);
+    var fuel_stations = formatter.fuel_stations;
+
+    $scope.fuels = formatter.station_counts.fuels;
+    $scope.total = formatter.station_counts.total;
+    $scope.stations = formatter.fuel_stations;
+
+    $scope.data = formatter;
 
     console.log("xhr.status: " + xhr.status);
     console.log("xhr.statusText: "+ xhr.statusText);
     console.log(xhr.body);
     console.log(xhr.response);
-    console.log("drones.status: " + drones.status);
-    console.log("drones.statusText: "+ drones.statusText);
-    console.log(drones.body);
-    console.log(drones.response);
 }]);
